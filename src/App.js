72294f30fe7da1b6df3cdf6c212a824fc9fce5e4
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-05-08 19:45:01 -0700

    added SearchBox component, modified App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+const state = {
+	robots: robots,
+	searchfield: ''
+};
+
+class App extends Component {
+	render() {
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox />
+				<CardList robots={robots} />
+			</div>
+		);
+	}
+}
+
+export default App;
